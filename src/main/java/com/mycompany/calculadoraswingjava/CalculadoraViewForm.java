176ec9f7f3befa0dc9869ede989386b042aa8e2f
mariacarolinass <m.carolina.soares1@gmail.com>
2022-05-18 10:30:54 -0300

    add componentes no painel form

--- src/main/java/com/mycompany/calculadoraswingjava/CalculadoraViewForm.java
+++ src/main/java/com/mycompany/calculadoraswingjava/CalculadoraViewForm.java
@@ -2,6 +2,7 @@ package com.mycompany.calculadoraswingjava;
 
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
+import java.awt.GridLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -51,17 +52,43 @@ public abstract class CalculadoraViewForm extends JFrame {
         
         this.getContentPane().setLayout(new BorderLayout());
         
-        // add painel rodape no fim do container
+        this.getContentPane().add(this.getPnlForm(), BorderLayout.CENTER);
         this.getContentPane().add(this.getPnlRodape(), BorderLayout.PAGE_END);
         
     }
-    
+
     
     // Gets
     public JPanel getPnlForm() {
         // criar novo painel se for nulo
         if (pnlForm == null) {
-            pnlForm = new JPanel();
+            pnlForm = new JPanel(new GridLayout(4, 2));
+            
+            this.jblCapital = new JLabel("Capital");
+            this.txtCapital = new JTextField(20);
+            
+            pnlForm.add(jblCapital);
+            pnlForm.add(txtCapital);
+            
+            this.jblTaxaJuros = new JLabel("Taxa Juros");
+            this.txtTaxaJuros = new JTextField(20);
+            
+            pnlForm.add(jblTaxaJuros);
+            pnlForm.add(txtTaxaJuros);
+            
+            this.jblPeriodo = new JLabel("Período");
+            this.txtPeriodo = new JTextField(20);
+            
+            pnlForm.add(jblPeriodo);
+            pnlForm.add(txtPeriodo);
+            
+            this.jblMontante = new JLabel("Montante");
+            this.txtMontante = new JTextField(20);
+            this.txtMontante.setEditable(false);
+            
+            pnlForm.add(jblMontante);
+            pnlForm.add(txtMontante);
+            
         }
         
         return pnlForm;
